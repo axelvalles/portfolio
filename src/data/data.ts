47bb06c14e3702c86081d0393ba2a05c09385651
axel valles <agabrielv1799@gmail.com>
2023-10-21 19:56:48 -0500

    initial commit

---
+++ src/data/data.ts
@@ -0,0 +1,39 @@
+export const experiences = [
+  {
+    title: "Simulador de tarifas TCC",
+    workPosition: "Full Stack Developer",
+    age: "2023",
+    content: [
+      "Durante mi labor como Full Stack Developer, participé en la creación de una aplicación interna para la empresa de envíos TCC, un simulador de tarifas. Esta aplicación tenía como objetivo principal ayudar a los equipos internos a calcular tarifas de envío de manera eficiente.",
+      "El Simulador de Tarifas TCC permitía a los empleados simular tarifas de envío al considerar diversas variables y modelos de tarifas específicos proporcionados por la empresa. Para lograr esto, desarrollé la aplicación web utilizando el framework Angular para el frontend, y el backend se implementó con Spring Boot en Java.",
+    ],
+    technologies: ["Angular", "Typescript", "Java", "Spring Boot"],
+  },
+  {
+    title: "NUAIO CX",
+    workPosition: "Frontend Developer",
+    age: "2022 - 2023",
+    content: [
+      "He tenido el privilegio de contribuir a la creación de una aplicación destinada al envío de campañas de marketing en Cadena. Estas herramientas se han centrado en brindar a las empresas una solución efectiva para conectar con su audiencia objetivo y fortalecer su presencia en línea. Mi rol en este proyecto involucró la integración de diversas funcionalidades diseñadas para personalizar mensajes de marketing y garantizar un seguimiento preciso del rendimiento de las campañas.",
+      "Uno de los desafíos más significativos que enfrentamos fue la creación de una interfaz amigable que permitiera a los usuarios construir flujos de campaña de manera intuitiva. Para lograr esto, desarrollamos una herramienta que les brindaba la capacidad de diseñar un árbol de nodos, donde cada nodo representaba una regla de envío. Este logro se hizo posible gracias al uso de diversas bibliotecas, siendo el núcleo de nuestra solución el framework React-flow-render.",
+    ],
+    technologies: [
+      "React",
+      "Typescript",
+      ".NET",
+      "Redux toolkit",
+      "React-flow-render",
+    ],
+  },
+  {
+    title: "Visualizador y editor de expedientes",
+    workPosition: "Frontend Developer",
+    age: "2021 - 2022",
+    content: [
+      "Trabaje en el desarrollo de una aplicación para la Registraduría Nacional de Colombia, donde desempeñé un papel fundamental en la creación de soluciones destinadas a optimizar la gestión de expedientes.",
+      "Esta aplicación fue meticulosamente diseñada con el propósito de simplificar y mejorar los procesos digitalización de la Registraduría específicamente para los juzgados de Popayán y Bucaramanga, lo que permitió un manejo más eficiente y seguro de la información. En este proyecto, mi enfoque se centró en el desarrollo Frontend utilizando Vue, lo que contribuyó a la interfaz amigable y efectiva de la aplicación.",
+      "Estoy orgulloso de haber contribuido a la modernización y mejora de los procedimientos de una institución tan importante como la Registraduría.",
+    ],
+    technologies: ["Vue", "Javascript", "Python"],
+  },
+];
