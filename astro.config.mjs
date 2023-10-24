41c6f62fbb34a2de44cfac19eef726fee134ca6c
axel valles <agabrielv1799@gmail.com>
2023-10-24 18:54:24 -0500

    feat: agregar seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,27 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import robotsTxt from "astro-robots-txt";
+
+import webmanifest from "astro-webmanifest";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.axelvalles.dev/",
-  integrations: [tailwind(), sitemap(), robotsTxt()]
+  integrations: [
+    tailwind(),
+    sitemap(),
+    robotsTxt(),
+    webmanifest({
+      name: "Axel Valles Dev",
+      icon: 'public/favicon.svg', // source for favicon & icons
+      short_name: "Axel Valles Dev",
+      description:
+        "Ingeniero en Informática experto en desarrollo Frontend de aplicaciones web modernas. Especializado en JavaScript y frameworks como Vue, React, Svelte, y Angular. Experiencia en SSR con Next y Nuxt, así como en aplicaciones mobiles con React Native y Flutter. Amplio conocimiento en el desarrollo Backend con Node.js y los frameworks Express y NestJS. ¡Descubre mi portafolio para explorar mis proyectos y habilidades!",
+      start_url: "/",
+      theme_color: "#6366f1",
+      background_color: "#1e1b4b",
+      display: "standalone",
+    }),
+  ],
 });
