126d356f11254f8e73ee17910f45ba93207bf770
axel valles <agabrielv1799@gmail.com>
2023-10-24 18:40:41 -0500

    feat: agregar seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: "https://www.axelvalles.dev/",
+  integrations: [tailwind(), sitemap(), robotsTxt()]
 });
